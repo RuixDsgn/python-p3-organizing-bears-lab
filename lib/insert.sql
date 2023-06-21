cfb8e8c82394eaf06b4ebc7e4f7baa0edfa97c81
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-20 17:07:38 -0700

    completed SQL lab with all tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 11, 'M', 'Black', 'Feisty', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'White', 'Reserved', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'Brown', 'Approachable', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'Mixed', 'Sweet', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'Green', 'Grumpy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Pink', 'Bubbly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'M', 'White', 'Shy', true);
